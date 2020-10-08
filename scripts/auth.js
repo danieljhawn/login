cd820e7867f1f9244308080b37580c5e1b942552
Daniel Hawn <danieljhawn@gmail.com>
2020-10-07 21:41:08 -0500

    login feature works

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,20 +1,44 @@
-//signup
-
+// signup
 const signupForm = document.querySelector('#signup-form');
 
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
-//get user info
+// get user info
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
-  //sign up the user
+  // sign up the user
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
     const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
   })
+});
 
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("user signed out");
+  });
+});
 
-})
+// login
+
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred)
+    // close login modal and reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  })
+});
