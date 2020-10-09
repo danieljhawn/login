f2b6f87aaa1ee339a26043838a8172f191e373a6
Daniel Hawn <danieljhawn@gmail.com>
2020-10-09 11:24:21 -0500

    hides guides for logged out users

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,14 +1,14 @@
-//  get data
-db.collection('guides').get().then(snapshot => {
-  setupGuides(snapshot.docs)
-})
+
 
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
-    console.log('user logged in: ', user.displayName)
+    //  get data
+    db.collection('guides').get().then(snapshot => {
+      setupGuides(snapshot.docs)
+    });
   } else {
-    console.log('user logged out')
+    setupGuides([]);
   }
 })
 
@@ -17,7 +17,7 @@ const signupForm = document.querySelector('#signup-form');
 
 signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
-// get user info
+  // get user info
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
